d09953c61fd817c43a2a65dce4535eab532799d5
Patrick <patrick204nqh@gmail.com>
2020-05-22 12:57:58 +0700

    fixed hide errors alert

--- src/app/app.js
+++ src/app/app.js
@@ -1,5 +1,7 @@
 // import { inputsAreValid } from './utils/inputs-are-valid';
 // import { parseInputs } from './utils/parse-inputs';
+const alertService = new AlertService();
+const componentService = new ComponentService();
 const run = (alertService, componentService) => {
   alertService.hideErrors();
 
@@ -16,3 +18,4 @@ const run = (alertService, componentService) => {
     }
   });
 };
+run(alertService, componentService);
