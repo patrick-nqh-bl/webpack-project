3e19333aa9232e892d084d93ce89fb04a0377376
Patrick <patrick204nqh@gmail.com>
2020-05-22 13:32:06 +0700

    adding 'export' & 'import' and remove file '.js' from index.html

--- src/app/alert.service.js
+++ src/app/alert.service.js
@@ -1,5 +1,5 @@
-// import { inputsAreValid } from './utils/inputs-are-valid';
-class AlertService {
+import { inputsAreValid } from './utils/inputs-are-valid';
+export class AlertService {
   constructor() {
     this.errorBox = document.getElementById('error');
   }
